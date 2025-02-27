1d66fc289a13eee3a0f7eef358e3531ae3dd2f28
daru <daru.darmakusuma@ecomindo.com>
2025-02-27 07:23:34 +0700

    chore: clean up env

--- platesWeb/src/lib/ChatArea.svelte
+++ platesWeb/src/lib/ChatArea.svelte
@@ -15,7 +15,7 @@
       scrollToBottom();
 
       try {
-        const response = await fetch("http://127.0.0.1:8000/chat", {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/chat`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ user_id, message: userMessage }),
